b4a864320ab70c9a0b472b319f6e21e2b2389660
wez <wezerr@yandex.ru>
2024-04-18 18:53:41 +0300

    fixe logo

--- packages/react/src/components/brand/BrandLogo.tsx
+++ packages/react/src/components/brand/BrandLogo.tsx
@@ -1,20 +1,8 @@
 import * as React from 'react';
 
 export class BrandLogo extends React.Component {
-  state = {
-    src: null,
-  };
-
-  // componentDidMount() {
-  //   const endpoint = 'My_endpoint_here';
-  //   fetch(endpoint)
-  //     .then((response) => response.json())
-  //     .then((data) => this.setState({ src: data.imageURL }))
-  //     .catch((error) => console.error('Error:', error));
-  // }
   
   render() {
-    const { src } = this.state;
     const parsBrandingData: string | null = sessionStorage.getItem('brandingData')
     let logoUrl = 'https://proofix.ru/wp-content/themes/proofix/assets/img/tild3435-6664-4661-b038-376665396631__group_26_2.png'
     if(parsBrandingData){
@@ -25,22 +13,14 @@ export class BrandLogo extends React.Component {
       
     }
     
-    if (!src)
-      return (
-        <div className="lk-branding_style">
-          <img
-            src={logoUrl}
-            style={{ width: '100%', height: 'auto' }}
-            alt="From API"
-          />
-        </div>
-      );
-
     return (
-      <img
-        src="https://img.freepik.com/premium-vector/abstract-colorful-bird_621127-276.jpg"
-        alt="From API"
-      />
+      <div className="lk-branding_style">
+        <img
+          src={logoUrl}
+          style={{ width: '100%', height: 'auto' }}
+          alt="From API"
+        />
+      </div>
     );
   }
 }
